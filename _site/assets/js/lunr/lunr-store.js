d95f4ada4d03ca430f25c71cd0a7dde0f1a2c213
Manuel Pasieka <manuel.pasieka@protonmail.ch>
2019-03-10 01:43:27 +0100

    Adding folders

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "First Post",
+        "excerpt":"First Post! ","categories": ["Some Category"],
+        "tags": ["Blog","Stuff"],
+        "url": "http://localhost:4000/some%20category/2018/07/17/first_post.html",
+        "teaser":null}]
